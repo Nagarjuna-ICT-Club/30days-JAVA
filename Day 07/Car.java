c926d6a0ad62fca6237f34ace4e76fd36ec22092
ManishK <mmann3536@gmail.com>
2025-08-13 09:08:27 +0545

    Day 2 to Day 7

---
+++ Day 07/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String brand;
+    int year;
+
+    void displayInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+}
---
+++ Day 07/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car(); // Creating object
+        myCar.brand = "Tesla";
+        myCar.year = 2024;
+
+        myCar.displayInfo(); // Calling method
+    }
+}
